05ba847659038fd97e3cc7f31755eecf575ac781
NakhleKaram <nakhlekaram1@gmail.com>
2022-01-31 20:03:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import imageback from'./assets/imageback.jpg';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+         <div  styles={{ backgroundImage:`url("./assets/imageback.jpg)` }}>
+               </div> 
+                </header>
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+     
     </div>
   );
 }
